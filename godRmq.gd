15c14ee9d51bc84f5931b5471056ef4f4d22d247
supermancho <supermancho@reddit>
2025-08-08 00:55:20 -0500

    send button and connected to incoming signal

--- godRmq.gd
+++ godRmq.gd
@@ -7,15 +7,16 @@ extends Node
 func _ready() -> void:
 	rmq.sConnected.connect(_on_server_connection)
 	rmq.sDisconnected.connect(_on_server_disconnected)
+	rmq.sRmqMsg.connect(_on_rmq_msg_heard)
 	return
 #-----
-func _on_send_pressed() -> void: # example data
+func _on_connect_pressed() -> void: # example data
 	var messageUsername:String = "uuser_1d6dc366-6936-4045-9599-a33f8f6ef1a9"
 	var messageCipher:String = "upass_password"
 	var publishingToQueueRk:String = "rk_user_publish_queue_1d6dc366-6936-4045-9599-a33f8f6ef1a9_5340245953529765175"
 	var consumingFromQueue:String = "user.receive.queue.1d6dc366-6936-4045-9599-a33f8f6ef1a9_5340245953529765175"
 	Log.info("clicked")
-	rmq.rmqConfigure(RMQValidatedConfig.new({
+	await rmq.rmqConnect(RMQValidatedConfig.new({
 		"rmqUsername": messageUsername,
 		"rmqCipher":  messageCipher,
 		"rmqVhost": C.RMQ_VHOST,
@@ -26,7 +27,6 @@ func _on_send_pressed() -> void: # example data
 		"rmqPort": C.RMQ_PORT,
 		"rmqConnectTimeoutSec": 5
 	}))
-	await rmq.start()
 	return
 #-----
 func _on_server_connection() -> void:
@@ -37,5 +37,14 @@ func _on_server_connection() -> void:
 func _on_server_disconnected(reason:String) -> void:
 	print("Server DISCONNECTED because " + reason)
 	status.text = "Got Disconnected"
+	return
+#-----
+func _on_rmq_msg_heard(json:String) -> void:
+	print("heard:" + json)
+	return
+#-----
+func _on_send_pressed() -> void:
+	if status.text == "Connected":
+		rmq.publish({"something":"special"})
 	return
 #-----
